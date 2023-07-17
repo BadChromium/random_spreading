7b89ab59b0fb2435fdf09221e41d6d2c0723ffa1
Zimeng <jimohoffman@gmail.com>
2023-07-17 16:22:50 +0800

    Added logger for GGT (see Circle_Graph_GGT.py)

---
+++ Circle_Graph_GGT.py
@@ -0,0 +1,79 @@
+# %%
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+from scipy.integrate import quad
+from scipy.stats import gamma
+import scipy.special as sc
+from logger import Logger
+import logging
+from functions import *
+
+# %% [markdown]
+# Considering a simple circle graph with equal weight, we can simply choose any of the nodes as the source node by symmetry.
+ggtlogger = Logger(
+    name='GGTlogger', 
+    loglevel='DEBUG', 
+    save=True, 
+    save_level=logging.INFO, 
+    log_path='./logs'
+    ).init_logger('1')
+# %%
+G = nx.Graph()
+G.add_nodes_from(['s', 'a', 'b', 'c', 'd'])
+G.add_edge('s', 'a', weight=0.5)
+G.add_edge('a', 'b', weight=0.5)
+G.add_edge('b', 'c', weight=0.5)
+G.add_edge('c', 'd', weight=0.5)
+G.add_edge('d', 's', weight=0.5)
+
+draw_graph(G)
+
+# %%
+# Initialization 
+s = np.random.choice(G.nodes()) # Randomly choose the source node for building the GGT. 
+# s = 's'
+print(f'The source node by randomly choosing is {s}.')
+I = [s]
+# Note the source here is not the same as that in rumor source detection.
+# Initialize the Gamma dist parameter for the source node and calculate E and Var.
+k_s = 1
+theta_s = 1
+E_s = k_s * theta_s
+V_s = k_s * theta_s**2
+# Build the GGT as T
+T = nx.Graph()
+T.add_node(s) # Add node s as the first parent node.
+# Create a dictionary for storing the values related to each node.
+k = {s:k_s}
+theta = {s:theta_s}
+E = {s:E_s}
+V = {s:V_s}
+
+
+while find_next_child(G, I) != None:
+    moment_dict = {}
+    moment2_dict = {}
+    child = find_next_child(G, I)
+    ggtlogger.info(f'The most possible child is {child}')
+    par_source = list(set(I) & set(G.neighbors(child)))
+    ggtlogger.info(f'The parent canditates are {par_source}')
+    for par_ in find_parents(child, G, par_source):
+        parents = find_parents(child, G, par_source)
+        moment_dict[par_] = compute_moment(child, k, theta, 1, parents)
+        moment2_dict[par_] = compute_moment(child, k, theta, 2, parents)
+    ggtlogger.debug(f'The current 1st moments are {moment_dict}')
+    par = min(moment_dict, key=moment_dict.get)
+    k[child] =compute_k(E[par], V[par], G.get_edge_data(par, child)['weight'])
+    theta[child] =compute_theta(E[par], V[par], G.get_edge_data(par, child)['weight'])
+    E[child] = moment_dict[par]
+    V[child] = moment2_dict[par] - moment_dict[par]**2
+    I.append(child)
+    T.add_edge(child, par, weight=round(compute_expectation(k[child], theta[child], k[par], theta[par]), 4))
+
+draw_graph(T)
+
+# %%
+MP_weighted(T, T.nodes(), T_max=5, num_T_positions=10)
+
+
---
+++ logger.py
@@ -0,0 +1,100 @@
+import logging
+import logging.handlers
+import os.path
+import sys
+import time
+
+LOG_FORMAT = logging.Formatter("%(asctime)s %(levelname)s %(name)s %(message)s")
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S %p"
+
+def empty_function(*args, **kargs):
+    pass
+
+logging.basicConfig = empty_function
+logging.raiseExceptions = True
+
+class Logger:
+    def __init__(self, name='Logger', loglevel='WARNING', save=True, save_level=logging.WARNING, log_path=None, update_frequency='D', save_interval=1):
+        self.log_path = log_path
+        self.name = name
+        self.loglevel = loglevel
+        self.log_level = None
+        self.save = save
+        self.save_level = save_level
+        self.logging = logging
+        self.log_format = LOG_FORMAT
+        self.date_format = DATE_FORMAT
+        self.update_frequency = update_frequency
+        self.save_interval = save_interval
+    
+    def init_streamHdl(self, formatter):
+        streamHandler = self.logging.StreamHandler(sys.stdout)
+        streamHandler.setLevel(self.log_level)
+        streamHandler.setFormatter(formatter)
+        return streamHandler
+    
+    def init_fileHdl(self, formatter, log_file_name='log.log'):
+        fileHandler = self.logging.FileHandler(log_file_name)
+        fileHandler.setLevel(self.save_level)
+        fileHandler.setFormatter(formatter)
+        return fileHandler
+    
+    def init_rotatingfileHdl(self, formatter, log_file_name='log.log'):
+        fh = self.logging.handlers.RotatingFileHandler(log_file_name, maxBytes=1024*1024*10, backupCount=5, mode='a')
+        fh.setLevel(self.save_level)
+        fh.setFormatter(formatter)
+        return fh
+    
+    def init_timedRotatingfileHdl(self, formatter, log_file_name='log.log'):
+        fh = self.logging.handlers.TimedRotatingFileHandler(log_file_name, when=self.update_frequency, interval=self.save_interval, backupCount=5)
+        fh.setLevel(self.save_level)
+        fh.setFormatter(formatter)
+        return fh
+    
+    def init_logger(self, fh_type='1'):
+        if self.loglevel == "DEBUG":
+            self.log_level = self.logging.DEBUG
+        elif self.loglevel == "INFO":
+            self.log_level = self.logging.INFO
+        elif self.loglevel == "WARNING":
+            self.log_level = self.logging.WARNING
+        elif self.loglevel == "ERROR":
+            self.log_level = self.logging.ERROR
+        else:
+            self.log_level = self.logging.CRITICAL
+
+        logger = self.logging.getLogger(self.name)
+        logger.setLevel(self.log_level)
+        formatter = self.logging.Formatter("%(asctime)s %(levelname)s %(name)s %(message)s")
+        logger.addHandler(self.init_streamHdl(formatter))
+
+        if self.save:
+            log_file = os.path.join(self.log_path, self.name + '.log')
+            if not os.path.exists(self.log_path):
+                os.makedirs(self.log_path)
+            if not os.path.exists(log_file):
+                os.system(r"touch {}".format(log_file))
+            if fh_type == '1':
+                fh = self.init_fileHdl(formatter, log_file)
+                logger.addHandler(fh)
+            elif fh_type == '2':
+                fh = self.init_rotatingfileHdl(formatter, log_file)
+                logger.addHandler(fh)
+            elif fh_type == '3':
+                fh = self.init_timedRotatingfileHdl(formatter, log_file)
+                logger.addHandler(fh)
+            return logger
+        
+if __name__ == '__main__':
+    try:
+        print(f"Number of params: {len(sys.argv)}")
+        print(f"Params: {str(sys.argv)}")
+        print(f"Script name: {sys.argv[0]}")
+        for item in range(1, len(sys.argv)):
+            print(f"Param {item}: {sys.argv[item]}")
+    except Exception as e:
+        print(e)
+    mylogger = Logger(name='test', loglevel='WARNING', save=True, save_level=logging.DEBUG, log_path='./logs').init_logger(fh_type='3')
+    mylogger.debug('debug message')
+    mylogger.info('info message')
+    mylogger.warning('warning message')
